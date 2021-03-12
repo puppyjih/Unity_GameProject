f6d6a44a2206d26f51b5d8fb160e1357ca3506e4
puppyjih <45137164+puppyjih@users.noreply.github.com>
2021-03-12 11:32:50 +0900

    Added Block Breaker Game

---
+++ Block Breaker/Assets/Scripts/PredictCount.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PredictCount : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI tmp;
+    private int guessCnt;
+    void Start()
+    {
+        //guessCnt = PlayerPrefs.GetInt("guessCnt");
+        //tmp.text = guessCnt.ToString();
+        //PlayerPrefs.Save();
+    }
+}
---
+++ Block Breaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void NextSceneLoader()
+    {
+        int curSceneNum = SceneManager.GetActiveScene().buildIndex;
+        int sceneNum = SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene((curSceneNum + 1)% sceneNum);
+    }
+}
